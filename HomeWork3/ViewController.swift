c6834d41b3047e93eab6986fdbd901993677ca90
Vladislav Starikov <yurqqqaa@icloud.com>
2023-10-01 13:22:27 +0300

    Initial Commit

---
+++ HomeWork3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork3
+//
+//  Created by Vladislav on 01.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
